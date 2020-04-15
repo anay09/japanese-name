93a74d13dd335202d645648335e6fba1b0af2244
anayc <anay.chopade>
2020-04-15 22:54:23 +0100

    Created requirements file Imported Flask framework into the code Created an introductory page as the default page when the server is opened Created a route so that when a username (i.e. a string) is typed into the URL, the framework outputs the English name and the Japanese equivalent

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask
+app = Flask(__name__)
+
+alphabet_map = {
+    'A': 'ka',
+    'B': 'tu',
+    'C': 'mi',
+    'D': 'te',
+    'E': 'ku',
+    'F': 'lu',
+    'G': 'ji',
+    'H': 'ri',
+    'I': 'ki',
+    'J': 'zu',
+    'K': 'me',
+    'L': 'ta',
+    'M': 'rin',
+    'N': 'to',
+    'O': 'mo',
+    'P': 'no',
+    'Q': 'ke',
+    'R': 'shi',
+    'S': 'ari',
+    'T': 'chi',
+    'U': 'do',
+    'V': 'ru',
+    'W': 'mei',
+    'X': 'na',
+    'Y': 'fu',
+    'Z': 'zi',
+}
+
+
+@app.route('/')
+def introductory_page():
+    return '<h1>------------English to Japanese Name Converter-----------'
+
+
+@app.route('/<username>/')
+def converter(username):
+    output_name = list()
+    [output_name.append(alphabet_map[i.upper()]) for i in username]
+    return 'Original Name: {i}, Translated Name: {o}'.format(
+            o=''.join(output_name),
+            i=username)
+
