80b3bdc92943b812c338fb55ff59ece74ef9c2ee
anayc <anay.chopade>
2020-04-19 19:29:57 +0100

    Created an output HTML page for the output of the program Created route for the output page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 alphabet_map = {
@@ -33,13 +33,21 @@ alphabet_map = {
 
 @app.route('/')
 def introductory_page():
-    return '<h1><marquee>-------------English to Japanese Name Converter----------------'
+    return render_template('index.html')
 
 
-@app.route('/<username>')
+@app.route('/', methods=['POST'])
+def output_page():
+    username = request.form['username']
+    jname = converter(username)
+    return render_template('output.html', username=username, jname=jname)
+
+
 def converter(username):
+    username = username.upper()
     output_name = list()
     [output_name.append(alphabet_map[i.upper()]) for i in username]
     return 'Original Name: {i}, Translated Name: {o}'.format(
             o=''.join(output_name),
             i=username)
+
